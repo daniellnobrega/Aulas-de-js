059c0cc15374ab99e2823e73947cf3b40c5a39cf
Daniel Nobrega <danielnobrega03@hotmail.com>
2022-11-22 20:35:29 -0300

    variasveis do tipo number

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const PontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number (não é um numero)
+
+const alura = "alura";
+
+console.log(alura * primeiroNumero);
