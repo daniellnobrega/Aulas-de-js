b722c38d4003dfcbf88bf7aeca75c533626d2e33
Daniel Nobrega <danielnobrega03@hotmail.com>
2022-11-24 22:35:02 -0300

    oque aprendemos de novo

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "daniel";
+const idade = 23;
+const cidadeDeNascimento = "são paulo";
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
+
+
